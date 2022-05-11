39ae8f946ae77f517f58c33f936f0b69265ef86a
Thiago Nepomuceno <thi.nepo@gmail.com>
2022-05-11 14:47:24 -0300

    refact: updating some files due to git project not created

--- setup.py
+++ setup.py
@@ -7,28 +7,29 @@ from wheel.bdist_wheel import bdist_wheel
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-# Remove build folder 
-#shutil.rmtree('build')
 
-def clean_folders():    
-    print('Removing temporary folders.')
+def clean_folders():
+    print("Removing temporary folders.")
 
-    TMP_FOLDERS = ['visualist.egg-info', 'build']
+    TMP_FOLDERS = ["visualist.egg-info", "build"]
     for folder in TMP_FOLDERS:
         shutil.rmtree(folder)
+
 
 class SdistCommand(sdist):
     """Custom build command."""
 
-    def run(self):        
+    def run(self):
         sdist.run(self)
+
 
 class BdistWheelCommand(bdist_wheel):
     """Custom build command."""
 
-    def run(self):        
+    def run(self):
         bdist_wheel.run(self)
         clean_folders()
+
 
 setuptools.setup(
     name="visualist",
@@ -39,22 +40,17 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     include_package_data=True,
-    url='https://github.com/ThiNepo/visualist',
-    #url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ThiNepo/visualist",
+    # url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
-    #entry_points={
+    # entry_points={
     #    'console_scripts': ['autoiot=bin.autoiot_cli:main'],
-    #},
+    # },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'Pillow>=7.1.1'
-    ],
-    cmdclass={
-        'sdist': SdistCommand,
-        'bdist_wheel': BdistWheelCommand
-    }
+    install_requires=["Pillow>=7.1.1"],
+    cmdclass={"sdist": SdistCommand, "bdist_wheel": BdistWheelCommand},
 )
--- visualist/__init__.py
+++ visualist/__init__.py
@@ -1,75 +1,165 @@
 from PIL import Image, ImageDraw, ImageFont
-import os 
+import os
 from typing import List
 
-class Visualist:
 
+class Visualist:
     def __init__(self, image_size=None, cell_size=64, space=32):
 
         self.CELL_SIZE = cell_size
         self.SPACE = space
 
-        font_path = os.path.join( os.path.abspath( os.path.dirname(__file__) ), 'fonts', 'Roboto-Medium.ttf' )
-        self.font = ImageFont.truetype(font_path, int(self.CELL_SIZE/2))
+        font_path = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "fonts", "Roboto-Medium.ttf"
+        )
+        self.font = ImageFont.truetype(font_path, int(self.CELL_SIZE / 2))
 
-    def img_from_list(self, L, highlight_indexes=[], image_size=None, color=(36, 116, 191, 255), highlight_color=(255, 46, 52, 255), indexes=True) -> Image:
+    def img_from_list(
+        self,
+        L,
+        highlight_indexes=[],
+        image_size=None,
+        color=(36, 116, 191, 255),
+        highlight_color=(255, 46, 52, 255),
+        show_indexes=True,
+        matrix=False
+    ) -> Image:
 
-        start_of_drawing = (len(L)/2)*(self.CELL_SIZE+self.SPACE)
+        start_of_drawing = (len(L) / 2) * (self.CELL_SIZE + self.SPACE)
         if image_size:
             IMAGE_SIZE = image_size
+        elif matrix:
+            IMAGE_SIZE = (int(2 * start_of_drawing), int(self.CELL_SIZE + self.SPACE))
         else:
-            IMAGE_SIZE = (int(2 * start_of_drawing), 
-                                int(3*self.CELL_SIZE))
+            IMAGE_SIZE = (int(2 * start_of_drawing), int(3 * self.CELL_SIZE))
 
-        img = Image.new('RGBA', IMAGE_SIZE, (255,255,255,255))        
+        img = Image.new("RGBA", IMAGE_SIZE, (255, 255, 255, 255))
 
-        MIDDLE = (img.size[0]/2, img.size[1]/2)
+        MIDDLE = (img.size[0] / 2, img.size[1] / 2)
 
         draw = ImageDraw.Draw(img)
-        pointer = MIDDLE[0] - start_of_drawing + self.SPACE/2
-        
+        pointer = MIDDLE[0] - start_of_drawing + self.SPACE / 2
+
         for index, item in enumerate(L):
             X0 = pointer
             X1 = pointer + self.CELL_SIZE
 
-            rect_color = color        
+            rect_color = color
             if index in highlight_indexes:
                 rect_color = highlight_color
-            draw.rectangle([(X0, MIDDLE[1] - self.CELL_SIZE/2), 
-                            (X1, MIDDLE[1] + self.CELL_SIZE/2)], fill=rect_color)
+            draw.rectangle(
+                [
+                    (X0, MIDDLE[1] - self.CELL_SIZE / 2),
+                    (X1, MIDDLE[1] + self.CELL_SIZE / 2),
+                ],
+                fill=rect_color,
+            )
 
             text_size = draw.textsize(str(item), font=self.font)
-            draw.text((pointer + self.CELL_SIZE/2 - text_size[0]/2, 
-                    MIDDLE[1] - self.CELL_SIZE/2 + text_size[1]/2), str(item), font=self.font)
-                
-            if indexes:
-                text_size = draw.textsize(str(index), font=self.font)
-                draw.text((pointer + self.CELL_SIZE/2 - text_size[0]/2, 
-                    MIDDLE[1] - self.CELL_SIZE - self.CELL_SIZE/2 + text_size[1]/2), 
-                    str(index), 
-                    font=self.font,
-                    fill=(0,0,0,255))
+            draw.text(
+                (
+                    pointer + self.CELL_SIZE / 2 - text_size[0] / 2,
+                    MIDDLE[1] - self.CELL_SIZE / 2 + text_size[1] / 2,
+                ),
+                str(item),
+                font=self.font,
+            )
 
-            pointer += self.CELL_SIZE + self.SPACE    
+            if show_indexes:
+                text_size = draw.textsize(str(index), font=self.font)
+                draw.text(
+                    (
+                        pointer + self.CELL_SIZE / 2 - text_size[0] / 2,
+                        MIDDLE[1]
+                        - self.CELL_SIZE
+                        - self.CELL_SIZE / 2
+                        + text_size[1] / 2,
+                    ),
+                    str(index),
+                    font=self.font,
+                    fill=(0, 0, 0, 255),
+                )
+
+            pointer += self.CELL_SIZE + self.SPACE
 
         return img
 
-    def img_from_lists(self,L, HI, image_size=None, color=(36, 116, 191, 255), highlight_color=(255, 46, 52, 255), indexes=True) -> Image:
+    def img_from_lists(
+        self,
+        L,
+        highlight_indexes,
+        image_size=None,
+        color=(36, 116, 191, 255),
+        highlight_color=(255, 46, 52, 255),
+        show_indexes=True,
+    ) -> Image:
         imgs = []
         sum_height = 0
         max_width = 0
-        for l, hi in zip(L, HI):
+        for l, hi in zip(L, highlight_indexes):
             if image_size is not None:
-                img = self.img_from_list(l, hi, (image_size[0], int(image_size[1]/len(L))), color, highlight_color, indexes)
+                img = self.img_from_list(
+                    l,
+                    hi,
+                    (image_size[0], int(image_size[1] / len(L))),
+                    color,
+                    highlight_color,
+                    show_indexes,
+                )
             else:
-                img = self.img_from_list(l, hi, image_size, color, highlight_color, indexes)
+                img = self.img_from_list(
+                    l, hi, image_size, color, highlight_color, show_indexes
+                )
             max_width = max(max_width, img.size[0])
             sum_height += img.size[1]
             imgs.append(img)
 
-        final_img = Image.new('RGBA', (max_width, sum_height), (255,255,255,255))   
+        final_img = Image.new("RGBA", (max_width, sum_height), (255, 255, 255, 255))
 
         for index, img in enumerate(imgs):
-            final_img.paste(img, (int(max_width/2 - img.size[0]/2), index*img.size[1]))
+            final_img.paste(
+                img, (int(max_width / 2 - img.size[0] / 2), index * img.size[1])
+            )
+
+        return final_img
+
+    def img_from_matrix(
+        self,
+        L,
+        HI,
+        image_size=None,
+        color=(36, 116, 191, 255),
+        highlight_color=(255, 46, 52, 255),
+        show_indexes=True,
+    ) -> Image:
+        imgs = []
+        sum_height = 0
+        max_width = 0        
+        for l, hi in zip(L, HI):
+            if image_size is not None:
+                img = self.img_from_list(
+                    l,
+                    hi,
+                    (image_size[0], int(image_size[1] / len(L))),
+                    color,
+                    highlight_color,
+                    False,
+                    True
+                )
+            else:
+                img = self.img_from_list(
+                    l, hi, image_size, color, highlight_color, False, True
+                )
+                
+            max_width = max(max_width, img.size[0])
+            sum_height += img.size[1]
+            imgs.append(img)
+
+        final_img = Image.new("RGBA", (max_width, sum_height), (255, 255, 255, 255))
+
+        for index, img in enumerate(imgs):
+            final_img.paste(
+                img, (int(max_width / 2 - img.size[0] / 2), index * img.size[1])
+            )
 
         return final_img
